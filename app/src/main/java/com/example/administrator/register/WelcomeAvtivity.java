8b7c39c8736b87a58cd61003384c7176db29de35
zhangyoyun <744120141@qq.com>
2015-12-25 09:51:36 +0800

    back版本

--- app/src/main/java/com/example/administrator/register/WelcomeAvtivity.java
+++ app/src/main/java/com/example/administrator/register/WelcomeAvtivity.java
@@ -1,13 +1,25 @@
 package com.example.administrator.register;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 
 public class WelcomeAvtivity extends Activity {
+    private Button back;
     protected void onCreate(Bundle savedInstanceState) {
         // TODO Auto-generated method stub
         super.onCreate(savedInstanceState);
         setContentView(R.layout.welcome);
+        back=(Button)findViewById(R.id.back);
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(WelcomeAvtivity.this, MainActivity.class);
+                WelcomeAvtivity.this.startActivity(intent);
+            }
+        });
     }
 }
